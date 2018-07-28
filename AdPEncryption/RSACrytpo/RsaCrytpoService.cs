b292ef50b852b6034a77dc7c46df731a7817c4bb
Anthony de Prince <Anthony.dePrince@ecs.be>
2018-07-28 11:51:58 +0200

    AdPEncryption - Basic RSA Encryption

---
+++ AdPEncryption/RSACrytpo/RsaCrytpoService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+using System.Text;
+
+namespace AdPEncryption.RSACrytpo
+{
+    public class RsaCrytpoService
+    {
+        private readonly RSACryptoServiceProvider _RsaCryptoServiceProvider;
+        private readonly RSAParameters _privateKey;
+        
+        public RsaCrytpoService()
+        {
+            _RsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
+            _privateKey = _RsaCryptoServiceProvider.ExportParameters(true);
+        }
+
+        public string GeneratePublicKey()
+        {
+            var publicKey = _RsaCryptoServiceProvider.ExportParameters(false);
+            using (var stringwriter = new StringWriter())
+            {
+                var xs = new XmlSerializer(typeof(RSAParameters));
+                xs.Serialize(stringwriter, publicKey);
+                return stringwriter.ToString();
+            }
+        }
+
+        public string Encrypt(string publicKey, string textToEncrypt)
+        {
+            RSAParameters pubKey;
+            using (var stringReader = new StringReader(publicKey))
+            {
+                var xs = new XmlSerializer(typeof(RSAParameters));
+                pubKey = (RSAParameters) xs.Deserialize(stringReader);
+            }
+            _RsaCryptoServiceProvider.ImportParameters(pubKey);
+            var bytesToEncrypt = Encoding.Unicode.GetBytes(textToEncrypt);
+            return Convert.ToBase64String(_RsaCryptoServiceProvider.Encrypt(bytesToEncrypt, RSAEncryptionPadding.Pkcs1));
+        }
+
+        public string Decrypt(string textToDecrypt)
+        {
+            var bytesToDecrypt = Convert.FromBase64String(textToDecrypt);
+            _RsaCryptoServiceProvider.ImportParameters(_privateKey);
+            var result = _RsaCryptoServiceProvider.Decrypt(bytesToDecrypt, RSAEncryptionPadding.Pkcs1);
+            return Encoding.Unicode.GetString(result);
+        }
+    }
+}
---
+++ AdPEncryptionTests/RSACrypto/RsaCryptoServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AdPEncryption.RSACrytpo;
+using NUnit.Framework;
+
+namespace AdPEncryptionTests.RSACrypto
+{
+    [TestFixture]
+    public class RsaCryptoServiceTest
+    {
+        [Test]
+        [Repeat(5)]
+        public void GeneratePublicKey_ReturnsPublicKey()
+        {
+            var rsaCryptoService = new RsaCrytpoService();
+            var pubKey = rsaCryptoService.GeneratePublicKey();
+            Console.WriteLine(pubKey);
+            Assert.IsNotNull(pubKey);
+        }
+
+        [Test]
+        [Repeat(5)]
+        public void Encrypt()
+        {
+            var rsaCryptoService = new RsaCrytpoService();
+            var textToEncrypt = "This Is Some Random Text To Encrypt";
+            var pubKey = rsaCryptoService.GeneratePublicKey();
+            var encryptedText = rsaCryptoService.Encrypt(pubKey, textToEncrypt);
+            Console.WriteLine(encryptedText);
+            Assert.IsNotNull(encryptedText);
+        }
+
+        [Test]
+        [Repeat(5)]
+        public void DeCrypt()
+        {
+            var rsaCryptoService = new RsaCrytpoService();
+            var textToEncrypt = "This Is Some Random Text To Encrypt";
+            var pubKey = rsaCryptoService.GeneratePublicKey();
+            var encryptedText = rsaCryptoService.Encrypt(pubKey, textToEncrypt);
+            var decryptedText = rsaCryptoService.Decrypt(encryptedText);
+            Console.WriteLine(decryptedText);
+            Assert.AreEqual(textToEncrypt, decryptedText);
+        }
+    }
+}
